aa058faa6280220880f3a0ecfd2e04de6a80bc07
AS-DB <adityasagarofficial2005@gmail.com>
2025-10-03 23:14:20 +0530

    Intial code work of flutter practical

---
+++ currency_converter/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+
+
+//creating a custom widget
+class MyApp extends StatelessWidget{
+@override
+  Widget build(BuildContext context) {
+    //return MaterialApp(Text("Hello Aditya",textDirection: TextDirection.rtl));
+
+    return MaterialApp(home: Scaffold(body: Text("hello Aditya"),)); 
+  }
+}
