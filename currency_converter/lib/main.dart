30e11df8343db9155448ee5a6e5be9aed9881fd6
ADITYA S DAS <adityasagarofficial2005@gmail.com>
2025-10-04 00:32:45 +0530

    Update main.dart

--- currency_converter/lib/main.dart
+++ currency_converter/lib/main.dart
@@ -14,10 +14,8 @@ class MyApp extends StatelessWidget{
     //return MaterialApp(Text("Hello Aditya",textDirection: TextDirection.rtl));
 
     return MaterialApp(home: Scaffold(body: Text("hello Aditya"),)); 
-<<<<<<< HEAD
     print("Hello adi");
-=======
     print("HEllo");
->>>>>>> 043234d21afddff992922d7cc71bcb5a102b86cf
+
   }
 }     
