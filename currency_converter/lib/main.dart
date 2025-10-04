ad8541399c06caeccc3342fe355c5b62b8866ded
AS-DB <adityasagarofficial2005@gmail.com>
2025-10-04 17:19:13 +0530

    Shifting text

--- currency_converter/lib/main.dart
+++ currency_converter/lib/main.dart
@@ -1,21 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
 
-
-
 //creating a custom widget
-class MyApp extends StatelessWidget{
-@override
+class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     //return MaterialApp(Text("Hello Aditya",textDirection: TextDirection.rtl));
 
-    return MaterialApp(home: Scaffold(body: Text("hello Aditya"),)); 
-    print("Hello adi");
-    print("HEllo");
+    //using MaterialApp component here
+    return MaterialApp(home: Scaffold(body: Text("hello Aditya")));
 
+    //shifting text in center
   }
-}     
+}
